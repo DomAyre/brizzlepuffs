a20af796121d3ec4f8355ac0126a15474e349d17
Dominic <domayre@outlook.com>
2016-02-23 13:26:21 +0000

    Styled Squad Cards

--- main.js
+++ main.js
@@ -19,8 +19,19 @@ $(function()
 
 $(window).resize(function() 
 {
+    scalePage();
+});
+
+function scalePage()
+{
     //Make the content occupy a greater proportion of the screen as it gets smaller
     var pivotalWidth = 1600;
-    var x = Math.max((pivotalWidth - $(window).width()), 0) / pivotalWidth;        
-    $(".centered").css({"width": 70+(x*20) + "%"});       
-});
+    var x = Math.max((pivotalWidth - $(window).width()), 0) / pivotalWidth;  
+    var contentWidth = 70+(x*20);      
+    $(".centered").css({"width": contentWidth + "%"});       
+    
+    //Set the padding size
+    var paddingPercentage = (100 - contentWidth)/2;
+    var paddingPixels = (paddingPercentage/100*$(window).width());
+    $(".padding").css({"width": paddingPixels + "px"});  
+}
